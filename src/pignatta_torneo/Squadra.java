24b61f0782ed13772b72562b286570512caf526b
TommyPigna <tommasopignatta@gmail.com>
2024-11-28 09:23:57 +0100

    Aggiunta delle classi Arbitro e Squadra

---
+++ src/pignatta_torneo/Squadra.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pignatta_torneo;
+
+/**
+ *
+ * @author Tommaso06
+ */
+public class Squadra {
+    
+}
