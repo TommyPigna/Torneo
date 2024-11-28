9ddf550b07b1246ce37c83c11a902be9741d0d11
TommyPigna <tommasopignatta@gmail.com>
2024-11-28 09:30:50 +0100

    Aggiunta del messaggio test nella classe di avvio

--- src/pignatta_torneo/Pignatta_Torneo.java
+++ src/pignatta_torneo/Pignatta_Torneo.java
@@ -14,7 +14,7 @@ public class Pignatta_Torneo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        System.out.println("AVVIO DEL PROGRAMMA");    
+        }
 }
+
